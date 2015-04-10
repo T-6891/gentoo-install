a3c484ff8a5ccc35bc0fe9ccf298f1e3fb118da8
T-6891 <master@T-brain.ru>
2015-04-10 12:29:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,28 +20,31 @@ case "$?" in
 esac
 
 # Выбор диска для установки
-fdisk -l | grep "Disk " | grep -v identifier | grep -v loop0 | awk '{print $2, $3 $4}' | sed -r 's/(.*),$/\1/' | awk '{print $0" off "}' > /tmp/DISKS
+DT=/tmp/DISKS$$
 
-NDISK1=`awk 'NR == 1' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE1=`awk 'NR == 1' /tmp/DISKS | awk '{print $2, $3}'`
-NDISK2=`awk 'NR == 2' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE2=`awk 'NR == 2' /tmp/DISKS | awk '{print $2, $3}'`
-NDISK3=`awk 'NR == 3' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE3=`awk 'NR == 3' /tmp/DISKS | awk '{print $2, $3}'`
-NDISK4=`awk 'NR == 4' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE4=`awk 'NR == 4' /tmp/DISKS | awk '{print $2, $3}'`
-NDISK5=`awk 'NR == 5' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE5=`awk 'NR == 5' /tmp/DISKS | awk '{print $2, $3}'`
-NDISK6=`awk 'NR == 6' /tmp/DISKS | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
-DSIZE6=`awk 'NR == 6' /tmp/DISKS | awk '{print $2, $3}'`
+fdisk -l | grep "Disk " | grep -v identifier | grep -v loop0 | awk '{print $2, $3 $4}' | sed -r 's/(.*),$/\1/' | awk '{print $0" off "}' > $DT 
+
+NDISK1=`awk 'NR == 1' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE1=`awk 'NR == 1' $DT | awk '{print $2, $3}'`
+NDISK2=`awk 'NR == 2' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE2=`awk 'NR == 2' $DT | awk '{print $2, $3}'`
+NDISK3=`awk 'NR == 3' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE3=`awk 'NR == 3' $DT | awk '{print $2, $3}'`
+NDISK4=`awk 'NR == 4' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE4=`awk 'NR == 4' $DT | awk '{print $2, $3}'`
+NDISK5=`awk 'NR == 5' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE5=`awk 'NR == 5' $DT | awk '{print $2, $3}'`
+NDISK6=`awk 'NR == 6' $DT | awk '{print $1}' | sed -r 's/(.*):$/\1/'`
+DSIZE6=`awk 'NR == 6' $DT | awk '{print $2, $3}'`
 
 DIALOG=${DIALOG=dialog}
 tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/sdisk$$
-trap "rm -f $tempfile" 0 1 2 5 15
+trap "rm -f $tempfile $DT" 0 1 2 5 15
 
 $DIALOG --backtitle "Preparing the disks" \
         --title "Selecting disks" --clear \
-        --radiolist "Please specify the drive on which the system will be installed " 20 61 5 \
+        --radiolist "Please specify the drive on which the system will be installed" 16 50 6 \
+        \
         $NDISK1 $DSIZE1 \
         $NDISK2 $DSIZE2 \
         $NDISK3 $DSIZE3 \
@@ -53,10 +56,9 @@ retval=$?
 
 choice=`cat $tempfile`
 case $retval in
-  0)
-    echo " ";;
   1)
     echo "Cancel pressed.";;
   255)
     echo "ESC pressed.";;
 esac
+
