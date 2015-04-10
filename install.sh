e919250a1f785768d99b483a891f29846352143e
T-6891 <master@T-brain.ru>
2015-04-10 14:36:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 # Импорт сетевых настроек
-dialog --title 'Network Settings' --yesno 'Do you want to import the active network settings?' 10 40
+DIALOG=${DIALOG=dialog}
+tempfile2=`tempfile2 2>/dev/null` || tempfile2=/tmp/snet$$
+tempfile3=`tempfile3 2>/dev/null` || tempfile3=/tmp/mnet$$
+trap "rm -f $tempfile2 $tempfile3" 0 1 2 5 15
 
-case "$?" in
-        '0')
+dialog --backtitle "Network Settings" --title "Network Settings" \
+--no-shadow --menu "Please import the active network settings or enter manually." 0 0 0 "import" "import the active network settings?" \
+"manual" "Manual Ip Setting" 2> $tempfile2
+
+retval=$?
+choice=`cat $tempfile2`
+case $choice in
+        "import")  
         IFACE=`ifconfig | grep RUNNING | awk '{print $1}' | sed -r 's/[:^]+//' | grep -v lo`
         IP=`ifconfig $IFACE | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 inet | grep -v inet`
         MASK=`ifconfig $IFACE  | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 netmask | grep -v netmask`
         GW=`netstat -rn | awk '{print $1, $2}' | grep "0.0.0.0 " | awk '{print $2}'`
         ;;
-        '1')
-        exit
-        ;;
-        '-1')
-        echo 'You came by pressing ESC, or inside the installer error occurred'
-        exit 1
+        "manual")
+        dialog --backtitle "Dialog Test Example" --title "IP Settings" \
+        --no-shadow --inputbox "EnterIP:" 0 0 "192.168.1.100" 2> $tempfile3
+        IPPI=`cat  $tempfile3`
         ;;
 esac
+
 
 # Выбор диска для установки
 DT=/tmp/DISKS$$
