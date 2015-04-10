b354ad5cef7f8c4fd2a763fbccea5f3f764ceda9
T-6891 <master@T-brain.ru>
2015-04-10 08:47:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Активный интерфейс
-IFACE=`ifconfig | grep RUNNING | awk '{print $1}' | sed -r 's/[:^]+//' | grep -v lo`
-
-# IP
-IP=`ifconfig $IFACE | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 inet | grep -v inet`
-
-# Netmask
-MASK=`ifconfig $IFACE  | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 netmask | grep -v netmask`
-
-# Шлюз по умолчанию
-GW=`netstat -rn | awk '{print $1, $2}' | grep "0.0.0.0 " | awk '{print $2}'`
-
-#!/bin/bash
-
-dialog --title 'Network Settings' --yesno 'Do you want to import the active network
-settings?' 10 40
+# Импорт сетевых настроек
+dialog --title 'Network Settings' --yesno 'Do you want to import the active network settings?' 10 40
 
 case "$?" in
         '0')
@@ -36,3 +22,4 @@ case "$?" in
         exit 1
         ;;
 esac
+
