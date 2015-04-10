c81cdd1f19fe2de9334b3c372c1cb9fcebdd5b2b
T-6891 <master@T-brain.ru>
2015-04-10 08:47:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ case "$?" in
         IP=`ifconfig $IFACE | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 inet | grep -v inet`
         MASK=`ifconfig $IFACE  | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 netmask | grep -v netmask`
         GW=`netstat -rn | awk '{print $1, $2}' | grep "0.0.0.0 " | awk '{print $2}'`
-        echo $IFACE
-        echo $IP
-        echo $MASK
-        echo $GW
         ;;
         '1')
         exit
