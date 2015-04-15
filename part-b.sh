f2894e7b92f803ea3f5443446df86698e8688e63
T-6891 <master@T-brain.ru>
2015-04-15 12:22:50 +0300

    Update part-b.sh

--- part-b.sh
+++ part-b.sh
@@ -2,3 +2,26 @@
 
 # Установка свежего среза портеджей
 emerge-webrsync
+
+# Настройка профиля
+eselect profile set default/linux/amd64/13.0
+
+# Настройка часового пояса
+echo "Europe/Moscow" > /etc/timezone
+
+# Применение настроек временной зоны
+emerge --config sys-libs/timezone-data
+
+# Настройка параметров локализации
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+
+# Генерация профилей локализации
+locale-gen
+
+# Выбор профиля локализации
+eselect locale set ru_RU.UTF-8
+
+# Обновление переменных окружения
+env-update && source /etc/profile
+
