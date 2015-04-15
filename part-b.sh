ab31460b3ca15a784abe6d7abbf979552706040d
T-6891 <master@T-brain.ru>
2015-04-15 12:41:01 +0300

    Update part-b.sh

--- part-b.sh
+++ part-b.sh
@@ -25,3 +25,41 @@ eselect locale set ru_RU.UTF-8
 # Обновление переменных окружения
 env-update && source /etc/profile
 
+# Установка исходных кодов ядра
+emerge -q sys-kernel/gentoo-sources
+
+# Настройка ядра
+cd /usr/src/linux
+wget http://piblic.t-brain.ru/conf/kernel-server -O /usr/src/linux/.config
+
+# Сборка нового ядра
+make && make modules_install
+
+# Установка нового ядра
+make install
+mkdir -p /boot/efi/boot
+cp /boot/vmlinuz* /boot/efi/boot/bootx64.efi
+
+# Настройка файловых систем
+echo $DISK1[1]   /boot        ext4    defaults,noatime     0 2 > /etc/fstab
+echo $DISK1[1]   none         swap    sw                   0 0 >> /etc/fstab
+echo $DISK1[1]   /            ext4    noatime              0 1 >> /etc/fstab
+echo  >> /etc/fstab
+echo $DISK1[1]   /dev/cdrom  /mnt/cdrom   auto    noauto,user          0 0 >> /etc/fstab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
