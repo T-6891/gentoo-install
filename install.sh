87992ad4e0963aadbf4f5d685732deb2a18d44e2
T-6891 <master@T-Brain.ru>
2015-04-28 15:48:04 +0300

    fix import network settings

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ retval=$?
 choice=`cat $temp2`
 case $choice in
         "import")  
-        IFACE=`ifconfig | grep RUNNING | awk '{print $1}' | sed -r 's/[:^]+//' | grep -v lo`
+        IFACE=`ifconfig | grep RUNNING | awk '{print $1}' | sed -r 's/[:^]+//' | grep -v lo | awk 'NR == 1'`
         IP=`ifconfig $IFACE | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 inet | grep -v inet`
         MASK=`ifconfig $IFACE  | grep inet | grep -v inet6 | grep -v "127.0.0.1" | awk -v RS=' ' '!(NR%1){gsub(/\n/," ");print} ' | grep -A 1 netmask | grep -v netmask`
         GW=`netstat -rn | awk '{print $1, $2}' | grep "0.0.0.0 " | awk '{print $2}'`
